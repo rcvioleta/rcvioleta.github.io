c46daaa7be51b4ae1f98cc6bd5c554dc4643e3d4
Rogene Cris Violeta <rcvioleta13@gmail.com>
2023-01-19 19:45:16 +0800

    Footer icons for mobile

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -2,9 +2,14 @@ import Link from "next/link";
 
 import styles from "./footer.module.css";
 
+import AsideLeft from "./aside-left";
+
 export default function Footer() {
   return (
     <footer id={styles.footer}>
+      <div className={styles["icons-container"]}>
+        <AsideLeft />
+      </div>
       <div className={styles.copyright}>
         <Link href="/">
           <p>Designed & Built by</p> <h2>Rogene Cris Violeta</h2>
