a0ac3ae989b3931786385ed3ae531263dc02868d
Rogene Cris Violeta <rcvioleta13@gmail.com>
2025-06-06 18:39:52 +0800

    Update font settings

--- components/sections/scrollable-section.js
+++ components/sections/scrollable-section.js
@@ -9,7 +9,7 @@ const Modal = ({ handleClose }) => {
 			<a href="#close" onClick={handleClose}>
 				X
 			</a>
-			<iframe
+			{/* <iframe
 				width="100%"
 				height="100%"
 				src="https://www.youtube.com/embed/UWwNIMHFdW4"
@@ -17,6 +17,15 @@ const Modal = ({ handleClose }) => {
 				frameborder="0"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 				referrerpolicy="strict-origin-when-cross-origin"
+				allowfullscreen
+			></iframe> */}
+			<iframe
+				width="640"
+				height="344"
+				src="https://www.loom.com/embed/c78ff21beee74a2ea84ed001ec01980c?sid=2c3a9dcb-4ea4-4f7c-b891-269d8106531b"
+				frameborder="0"
+				webkitallowfullscreen
+				mozallowfullscreen
 				allowfullscreen
 			></iframe>
 		</div>
@@ -154,6 +163,22 @@ export default function ScrollableSection() {
 						href="/resume.pdf"
 					>
 						View Full Resume
+						<span style={{ display: "inline-block" }}>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								class="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+								aria-hidden="true"
+								data-darkreader-inline-fill=""
+							>
+								<path
+									fill-rule="evenodd"
+									d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+									clip-rule="evenodd"
+								></path>
+							</svg>
+						</span>
 					</a>
 				</div>
 			</section>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import Head from "next/head";
 
+import { useEffect } from "react";
 import ScrollableSection from "../components/sections/scrollable-section";
 
 const GTAG_ID = process.env.GTAG_ID;
 
 export default function Home() {
+	useEffect(() => {
+		const handleMouseOver = (e) => {
+			const x = Math.round((e.pageX / window.innerWidth) * 100);
+			const y = Math.round((e.pageY / window.innerHeight) * 46);
+
+			document.body.style.backgroundImage = `radial-gradient(37.5rem at ${x}% ${y}%, #0f0f0f, #000000)`;
+		};
+
+		window.addEventListener("mousemove", handleMouseOver);
+
+		return () => window.removeEventListener("mousemove", handleMouseOver);
+	}, []);
+
 	return (
 		<>
 			<Head>
