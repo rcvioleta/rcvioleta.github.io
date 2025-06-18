111bc6a22768ac85d949057e2fb8662b53387a9d
Rogene Cris Violeta <rcvioleta13@gmail.com>
2025-06-18 19:05:51 +0800

    Implement half-page crack design

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
 import Head from "next/head";
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import ScrollableSection from "../components/sections/scrollable-section";
 
 const GTAG_ID = process.env.GTAG_ID;
 
 export default function Home() {
-	useEffect(() => {
-		const handleMouseOver = (e) => {
-			const x = Math.round((e.pageX / window.innerWidth) * 100);
-			const y = Math.round((e.pageY / window.innerHeight) * 46);
-			const background = `radial-gradient(37.5rem at ${x}% ${y}%, #0f0f0f, #000000)`;
+	// useEffect(() => {
+	// 	const handleMouseOver = (e) => {
+	// 		const x = Math.round((e.pageX / window.innerWidth) * 100);
+	// 		const y = Math.round((e.pageY / window.innerHeight) * 46);
+	// 		const background = `radial-gradient(37.5rem at ${x}% ${y}%, #0f0f0f, #000000)`;
 
-			document.body.style.backgroundImage = background;
-		};
+	// 		document.body.style.backgroundImage = background;
+	// 	};
 
-		window.addEventListener("mousemove", handleMouseOver);
+	// 	window.addEventListener("mousemove", handleMouseOver);
 
-		return () => window.removeEventListener("mousemove", handleMouseOver);
-	}, []);
+	// 	return () => window.removeEventListener("mousemove", handleMouseOver);
+	// }, []);
 
 	return (
 		<>
